151cb49665a5ed7ccf9aa1795b8a5190afd9f311
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:40:35 +0100

    Update LSEOrdenada.java

--- src/Estructuras/LSE/LSEOrdenada.java
+++ src/Estructuras/LSE/LSEOrdenada.java
@@ -1,4 +1,28 @@
 package Estructuras.LSE;
 
-public class LSEOrdenada {
+public class LSEOrdenada<T extends Comparable<T>> extends ListaSimplementeEnlazada<T> {
+
+    @Override
+    public void add(T dato) {
+        ElementoSE<T> nuevo = new ElementoSE<>(dato);
+
+        // Caso 1: Lista vacía o el dato es menor que el primero
+        if (isEmpty() || dato.compareTo(primero.dato) < 0) {
+            nuevo.siguiente = primero;
+            primero = nuevo;
+        }
+        // Caso 2: Buscar su posición en medio o al final
+        else {
+            ElementoSE<T> ant = primero;
+            ElementoSE<T> act = primero.siguiente;
+
+            while (act != null && dato.compareTo(act.dato) > 0) {
+                ant = act;
+                act = act.siguiente;
+            }
+            nuevo.siguiente = act;
+            ant.siguiente = nuevo;
+        }
+        tamaño++;
+    }
 }
