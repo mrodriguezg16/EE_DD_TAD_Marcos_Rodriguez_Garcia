6c79821449563845450ae128cedd2323eab7e000
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:42:26 +0100

    Update IteradorCircular.java

--- src/Estructuras/ListaCircular/Basado_en_LDE/IteradorCircular.java
+++ src/Estructuras/ListaCircular/Basado_en_LDE/IteradorCircular.java
@@ -1,4 +1,28 @@
 package Estructuras.ListaCircular.Basado_en_LDE;
 
-public class IteradorCircular {
+import Estructuras.Interfaces.MiIterador;
+
+public class IteradorCircular<T> implements MiIterador<T> {
+    private ElementoDE<T> actual;
+    private int elementosVisitados;
+    private int total;
+
+    public IteradorCircular(ElementoDE<T> comienzo, int total) {
+        this.actual = comienzo;
+        this.total = total;
+        this.elementosVisitados = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return elementosVisitados < total;
+    }
+
+    @Override
+    public T next() {
+        T valor = actual.dato;
+        actual = actual.siguiente;
+        elementosVisitados++;
+        return valor;
+    }
 }
