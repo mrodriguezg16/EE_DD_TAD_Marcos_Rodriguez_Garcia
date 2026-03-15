2b120272eecbf854e1dc8f4ebb238e06e5d5fa4a
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:37:29 +0100

    Update ElementoDE.java

--- src/Estructuras/LDE/ElementoDE.java
+++ src/Estructuras/LDE/ElementoDE.java
@@ -1,4 +1,13 @@
 package Estructuras.LDE;
 
-public class ElementoDE {
+public class ElementoDE<T> {
+    T dato;
+    ElementoDE<T> siguiente;
+    ElementoDE<T> anterior; // Nuevo componente para la lista doble
+
+    public ElementoDE(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
 }
