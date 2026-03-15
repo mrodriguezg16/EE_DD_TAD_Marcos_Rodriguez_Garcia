51ffc311df37226a4ad47567d32647cafa152b71
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:37:54 +0100

    Update IteradorLDE.java

--- src/Estructuras/LDE/IteradorLDE.java
+++ src/Estructuras/LDE/IteradorLDE.java
@@ -1,4 +1,23 @@
 package Estructuras.LDE;
 
-public class IteradorLDE {
+import Estructuras.Interfaces.MiIterador;
+
+public class IteradorLDE<T> implements MiIterador<T> {
+    private ElementoDE<T> actual;
+
+    public IteradorLDE(ElementoDE<T> comienzo) {
+        this.actual = comienzo;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return actual != null;
+    }
+
+    @Override
+    public T next() {
+        T valor = actual.dato;
+        actual = actual.siguiente;
+        return valor;
+    }
 }
