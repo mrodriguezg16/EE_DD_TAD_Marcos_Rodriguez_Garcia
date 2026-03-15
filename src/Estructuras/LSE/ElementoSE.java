5c185f30a642f90681ab57ec15177dd5843e80c9
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:39:44 +0100

    Update ElementoSE.java

--- src/Estructuras/LSE/ElementoSE.java
+++ src/Estructuras/LSE/ElementoSE.java
@@ -1,4 +1,12 @@
 package Estructuras.LSE;
 
-public class ElementoSE {
+public class ElementoSE<T> {
+    public T dato;           //El dato que vamos a introducir.
+    public ElementoSE<T> siguiente; // El enlace al próximo nodo (El puntero por asi decirlo).
+
+    // Constructor sencillo
+    public ElementoSE(T valor) {
+        this.dato = valor; //Cabeza de nuestra lista.
+        this.siguiente = null; //Al principio, como solo hay 1 elemento, nuestro puntero no apunta a nadie,
+    }                          // no hay un siguiente elemento (null).
 }
