39951fe534e96e4dfd288c18746068b3d4e8e308
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:35:48 +0100

    Update ElementoDE.java

--- src/Estructuras/Colas/ElementoDE.java
+++ src/Estructuras/Colas/ElementoDE.java
@@ -1,4 +1,13 @@
 package Estructuras.Colas;
 
-public class ElementosDE {
+public class ElementoDE<T> { //Reutilizamos el nodo de las listas doblemente enlazadas.
+    T dato;
+    ElementoDE<T> siguiente;
+    ElementoDE<T> anterior; // Nuevo componente para la lista doble
+
+    public ElementoDE(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
 }
