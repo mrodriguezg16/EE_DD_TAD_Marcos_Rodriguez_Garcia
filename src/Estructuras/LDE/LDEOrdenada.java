4cc883e74abdb653ccc57eb0e769c68a3525bae1
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:39:14 +0100

    Update LDEOrdenada.java

--- src/Estructuras/LDE/LDEOrdenada.java
+++ src/Estructuras/LDE/LDEOrdenada.java
@@ -1,4 +1,38 @@
 package Estructuras.LDE;
 
-public class LDEOrdenada {
+public class LDEOrdenada<T extends Comparable<T>> extends ListaDoblementeEnlazada<T> {
+
+    @Override
+    public void add(T dato) {
+        ElementoDE<T> nuevo = new ElementoDE<>(dato);
+
+        if (isEmpty()) {
+            primero = ultimo = nuevo;
+        }
+        // Caso: insertar al principio (el dato es menor que el primero)
+        else if (dato.compareTo(primero.dato) < 0) {
+            nuevo.siguiente = primero;
+            primero.anterior = nuevo;
+            primero = nuevo;
+        }
+        // Caso: insertar al final (el dato es mayor o igual que el último)
+        else if (dato.compareTo(ultimo.dato) >= 0) {
+            ultimo.siguiente = nuevo;
+            nuevo.anterior = ultimo;
+            ultimo = nuevo;
+        }
+        // Caso: insertar en medio
+        else {
+            ElementoDE<T> aux = primero;
+            while (aux != null && dato.compareTo(aux.dato) > 0) {
+                aux = aux.siguiente;
+            }
+            // Insertamos antes de 'aux'
+            nuevo.siguiente = aux;
+            nuevo.anterior = aux.anterior;
+            aux.anterior.siguiente = nuevo;
+            aux.anterior = nuevo;
+        }
+        tamaño++;
+    }
 }
