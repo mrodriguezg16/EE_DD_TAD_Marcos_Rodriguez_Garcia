dd0cb309e2df5cf2f47ee8623ecb9228d4c03533
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:40:03 +0100

    Update IteradorLSE.java

--- src/Estructuras/LSE/IteradorLSE.java
+++ src/Estructuras/LSE/IteradorLSE.java
@@ -1,4 +1,24 @@
 package Estructuras.LSE;
 
-public class IteradorLSE {
+import Estructuras.Interfaces.MiIterador;
+
+public class IteradorLSE<T> implements MiIterador<T> {
+    private ElementoSE<T> actual;
+
+    // El constructor recibe el comienzo de la lista
+    public IteradorLSE(ElementoSE<T> comienzo) {
+        this.actual = comienzo;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return actual != null;
+    }
+
+    @Override
+    public T next() {
+        T dato = actual.dato;
+        actual = actual.siguiente;
+        return dato;
+    }
 }
