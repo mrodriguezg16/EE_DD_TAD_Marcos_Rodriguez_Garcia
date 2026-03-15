7cb7385ade63ae4f2945c7b74e04790a76219fd6
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-15 17:36:21 +0100

    Update Lista.java

--- src/Estructuras/Interfaces/Lista.java
+++ src/Estructuras/Interfaces/Lista.java
@@ -1,4 +1,19 @@
 package Estructuras.Interfaces;
 
-public interface Lista {
+// Interfaz para la Lista de todos los TAD
+public interface Lista<T extends Comparable<T>> {
+    void add(T dato);
+    T get(T dato);
+    T del(T dato);
+    boolean isEmpty();
+    int getSize();
+    MiIterador<T> getIterador();
+
+    // Los extras para subir nota (fáciles de leer)
+    void vaciar();
+    boolean existe(T dato);
+    T obtenerPorPosicion(int puesto);
+    void cambiarEnPosicion(int puesto, T nuevoDato);
+    String mostrarLista(); // Un toString pero con nombre más sencillo
 }
+
