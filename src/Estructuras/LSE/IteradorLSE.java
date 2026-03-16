b17a07e1fd81f3da9551d6ea8697542f613bb133
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-16 20:30:53 +0100

    Update IteradorLSE.java

--- src/Estructuras/LSE/IteradorLSE.java
+++ src/Estructuras/LSE/IteradorLSE.java
@@ -1,7 +1,7 @@
 package Estructuras.LSE;
 
 import Estructuras.Interfaces.MiIterador;
-
+//Clase iterador para poder recorrer nuestra lista.
 public class IteradorLSE<T> implements MiIterador<T> {
     private ElementoSE<T> actual;
 
@@ -9,12 +9,13 @@ public class IteradorLSE<T> implements MiIterador<T> {
     public IteradorLSE(ElementoSE<T> comienzo) {
         this.actual = comienzo;
     }
-
+//Detecta si hay un siguiente elemento
     @Override
     public boolean hasNext() {
         return actual != null;
     }
-
+//Si hay siguiente, devuelve por pantalla el elemento, salta al siguiente, y si detecta otro siguiente repite el ciclo hasta recorrer la lista entera
+//(y no queden mas elementos siguientes)
     @Override
     public T next() {
         T dato = actual.dato;
