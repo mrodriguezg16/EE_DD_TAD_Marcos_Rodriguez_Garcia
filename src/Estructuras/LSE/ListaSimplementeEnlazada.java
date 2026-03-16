eb4a98a160ae2f5cabc318972ca63bf07322939f
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-16 21:21:59 +0100

    Update ListaSimplementeEnlazada.java

--- src/Estructuras/LSE/ListaSimplementeEnlazada.java
+++ src/Estructuras/LSE/ListaSimplementeEnlazada.java
@@ -2,16 +2,19 @@ package Estructuras.LSE;
 
 import Estructuras.Interfaces.Lista;
 import Estructuras.Interfaces.MiIterador;
-
+//Diseño modular: En este planteamiento, hemos decidido plantear los TAD de modo que se pueda reutilizar el código de forma optima
+// Para ello, hemos establecido interfaces para las listas y el iterador, así como la clase Elemento en todos los TAD de forma que todos ellos
+// la usan, cabe destacar que hemos creado 2 clases elemento: una correspondiente con
+//los elementos simplemente enlazados, y otra con los doblemente enlazados (Las pilas y colas tambien usan esas clases).
 public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<T> {
     protected ElementoSE<T> primero; // El símbolo rombo (◊) es protected
     protected int tamaño;
-
+//Constructor de lista vacía
     public ListaSimplementeEnlazada() {
         this.primero = null;
         this.tamaño = 0;
     }
-
+//Método que permite añadir a la lista un elemento.
     @Override
     public void add(T dato) {
         ElementoSE<T> nuevo = new ElementoSE<>(dato);
@@ -24,9 +27,9 @@ public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<
             }
             aux.siguiente = nuevo;
         }
-        tamaño++;
+        tamaño++;//Aumenta el tamaño siempre que añadimos un nuevo elemento.
     }
-
+//Getter
     @Override
     public T get(T dato) {
         ElementoSE<T> aux = primero;
@@ -36,7 +39,7 @@ public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<
         }
         return null;
     }
-
+//Método para eliminar elementos de la lista.
     @Override
     public T del(T dato) {
         // Lógica sencilla de borrado
@@ -50,19 +53,19 @@ public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<
         // ... búsqueda y borrado del nodo intermedio ...
         return null;
     }
-
+//Método que analiza si la lista está vacía
     @Override
     public boolean isEmpty() { return primero == null; }
-
+//Método que devuelve el tamaño de la lista
     @Override
     public int getSize() { return tamaño; }
-
+//Método que nos permite recorrer la lista
     @Override
     public MiIterador<T> getIterador() {
         return new IteradorLSE<>(primero);
     }
 
-    // Borra todo de golpe
+    //Método que borra todo de golpe
     @Override
     public void vaciar() {
         this.primero = null;
@@ -70,7 +73,7 @@ public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<
         System.out.println("La lista se ha vaciado.");
     }
 
-    // Dice si un dato está o no
+    //Método que dice si un dato está o no
     @Override
     public boolean existe(T dato) {
         ElementoSE<T> aux = primero;
@@ -83,7 +86,7 @@ public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<
         return false; // Si llega aquí, es que no estaba
     }
 
-    // Busca por el número de la "cajita" (0, 1, 2...)
+    //Método que busca por el número de la posición (0, 1, 2...)
     @Override
     public T obtenerPorPosicion(int puesto) {
         if (puesto < 0 || puesto >= tamaño) {
@@ -99,7 +102,7 @@ public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<
         return aux.dato;
     }
 
-    // Cambia el dato de una cajita específica
+    //Método que cambia el dato de una posición a otra
     @Override
     public void cambiarEnPosicion(int puesto, T nuevoDato) {
         if (puesto < 0 || puesto >= tamaño) {
@@ -114,7 +117,7 @@ public class ListaSimplementeEnlazada<T extends Comparable<T>> implements Lista<
         aux.dato = nuevoDato; // Sobreescribimos el dato viejo
     }
 
-    // Crea un texto para imprimir la lista en consola
+    //Método que muestra un texto para imprimir la lista en consola
     @Override
     public String mostrarLista() {
         if (isEmpty()) {
